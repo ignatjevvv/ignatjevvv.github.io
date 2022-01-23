1a67d8f25c57d2b4c6f01cab0720b4381a54244b
Konstantin Ignatjev <ignatjev.konstantin@gmail.com>
2022-01-23 02:45:50 +0200

    Update main.js

--- converter/js/main.js
+++ converter/js/main.js
@@ -3,7 +3,9 @@ const redgateXML = document.getElementById('redgateXML');
 const button = document.getElementById('button');
 const countItem = document.querySelectorAll('.count');
 
-button.addEventListener('click', () => {
+button.addEventListener('click', createItemList);
+
+function createItemList() {
 
    let shopNameArr = [];
 
@@ -11,65 +13,86 @@ button.addEventListener('click', () => {
       shopNameArr = shopName.value
          .replace(/\n/g, ' ')
          .split(' ')
-         .filter( function(el) {
-         return el != '';
-      });
+         .filter(function (el) { return el != '' });
    }
 
-   shopName.value = '';
-   redgateXML.value = '';
+   deleteDublicateItem(shopNameArr);
+}
 
-   shopNameArr.forEach(function (i) {
-      if (i != '' && i.indexOf('eva') != 0) {
-         shopName.value += `eva${i.replace(/\n/g, '')}a\n`;
-         redgateXML.value += `<value version = "5" type = "database">
-    <name>master</name> 
-    <server>eva${i.replace(/\n/g, '')}a\\kassa</server> 
-    <integratedSecurity>False</integratedSecurity> 
-    <username>sa</username> 
-    <savePassword>True</savePassword> 
-    <password encrypted="1">UKjJDz3jUrg=</password> 
-    <connectionTimeout>15</connectionTimeout> 
-    <protocol>-1</protocol> 
-    <packetSize>4096</packetSize> 
-    <encrypted>False</encrypted> 
-    <selected>False</selected> 
-    <cserver>eva${i.replace(/\n/g, '')}a\\kassa</cserver> 
-</value> \n`
-      }
+function deleteDublicateItem(arr) {
+
+   const arrayWithoutDuplicates = [...new Set(arr)];
+   let duplicates = [...arr];
+
+   arrayWithoutDuplicates.forEach((item) => {
+      const i = duplicates.indexOf(item);
+      duplicates = duplicates
+         .slice(0, i)
+         .concat(duplicates.slice(i + 1, duplicates.length));
    });
 
-   countItem[0].innerHTML = `Количество: ${shopNameArr.length}`;
-   shopNameArr.splice(0);
+   showListItem(arrayWithoutDuplicates);
+   showCountItem(arrayWithoutDuplicates);
 
-})
+}
 
+function showListItem(shopListArr) {
 
+   shopName.value = "";
+   redgateXML.value = "";
 
-/********************* COPY TO BUFER ********************/
+   shopListArr.forEach(function (i) {
 
-const copyLabel = document.querySelectorAll('.copy');
+      if (i != '' && i.indexOf('eva') != 0) {
+
+         shopName.value += `eva${i.replace(/\n/g, '')}a\n`;
+         redgateXML.value +=
+            `<value version = "5" type = "database">` +
+            `\n   <name>master</name>` +
+            `\n   <server>eva${i.replace(/\n/g, '')}a\\kassa</server>` +
+            `\n   <integratedSecurity>False</integratedSecurity>` +
+            `\n   <username>sa</username>` +
+            `\n   <savePassword>True</savePassword>` +
+            `\n   <password encrypted="1">UKjJDz3jUrg=</password>` +
+            `\n   <connectionTimeout>15</connectionTimeout>` +
+            `\n   <protocol>-1</protocol>` +
+            `\n   <packetSize>4096</packetSize>` +
+            `\n   <encrypted>False</encrypted>` +
+            `\n   <selected>False</selected>` +
+            `\n   <cserver>eva${i.replace(/\n/g, '')}a\\kassa</cserver>` +
+            `\n</value>\n`
+      }
+
+   });
+
+}
+
+function showCountItem(arr) {
+   countItem[0].innerHTML = `Количество: ${arr.length}`;
+}
+
+/********************* COPY TO BUFFER ********************/
+
 const copyButton = document.querySelectorAll('.wrapper__copy');
 
+copyButton.forEach((copyBtn, i) => {
+   copyBtn.addEventListener('click', () => {
+      if (shopName.value.includes('eva')) {
+         copyBtn.parentNode.childNodes[7].select();
+         document.execCommand("copy")
+         modal(i);
+         setTimeout(modalClose, 1000);
+      }
+   })
+});
+
+/**************** MODAL LABEL "Copyed!" *****************/
+
+const copyLabel = document.querySelectorAll('.copy');
 
 let modal = function (label) {
    copyLabel[label].classList.add('active');
 }
-
-copyButton.forEach((copyBtn, i) => {
-   copyBtn.addEventListener('click', () => {
-      console.log(i);
-
-      copyBtn.parentNode.childNodes[7].select();
-      document.execCommand("copy");
-
-      if (shopName.textLength > 0) {
-         modal(i);
-         setTimeout(modalClose, 1000);
-      }
-
-   })
-});
 
 let modalClose = function () {
    copyLabel.forEach(copyLabel => {
