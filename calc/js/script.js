b6c45c86eff9a1e2b79261b62583142e591e58d5
Konstantin Ignatjev <ignatjev.konstantin@gmail.com>
2022-05-09 06:12:50 +0300

    Delete calc/js directory

--- calc/js/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const calc = require('./modules/calc'),
-          elect = require('./modules/elect'),
-          water = require('./modules/water');
-
-          calc();
-          elect();
-          water();
-});
